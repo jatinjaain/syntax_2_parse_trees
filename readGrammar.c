bcec3a8572dce5b00b9e30f60029df8ec9fec282
jaitnjaain <jaitnjaain@gmail.com>
2020-10-24 01:12:03 +0530

    pushing readgrammar for morning bikash

--- readGrammar.c
+++ readGrammar.c
@@ -3,3 +3,41 @@
  * that reads grammar from grammar.txt,
  * populates array of linked-list grammar G 
 */
+#include "grammar_definitions.h"
+#include "structures.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+Grammar_Node *readGrammar(char *, Grammar_Node[]);
+
+Grammar_Node *readGrammar(char *filename, Grammar_Node G[])
+{
+
+	FILE *fptr = fopen(filename, "r");
+
+	if ((fptr) == NULL)
+	{
+		printf("Error! opening file");
+		// Program exits if the file pointer returns NULL.
+		exit(1);
+	}
+	
+	G = (Grammar_Node *)malloc(MAX_RULES_NUM * sizeof(Grammar_Node));
+	for (int i = 0;; i++)
+	{
+		char str[MAX_NAME_LEN];
+		char next_str[MAX_NAME_LEN];
+		fscanf(fptr,"%s", &str[0]);
+		if (strcmp(next_str, ":=") == 0)
+		{
+			//put str in the node
+		}
+		if (strcmp(str, "|") == 0)
+		{
+			//put G[i] in the G[i+1]
+		}
+
+		Grammar_Node *temp = G;
+		fclose(fptr);
+	}
