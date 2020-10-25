6c5a993c700fa181657f1301723d589ebb65e7d6
CyanideForBreakfast <dwivedi.utkarsh01@gmail.com>
2020-10-25 20:56:48 +0530

    create driver file

---
+++ driver.c
@@ -0,0 +1,23 @@
+#include "structures.h"
+#include "readGrammar.c"
+#include "parser.c"
+
+int main(){
+    int option;
+    while(1){
+        scanf("%d",&option);
+        switch(option){
+            case 0:
+                exit(1);
+                break;
+            case 1:
+                //create parse trees
+            case 2:
+                //transverse parse trees
+            case 3:
+                //print parse trees
+            case 4:
+                //print type expression table
+        }
+    } 
+}
