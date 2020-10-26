ac8301db437bad08d286326b3c465c5e35f0c9ea
7d58c9638beb9f15d8ffbfd170a16bd0c2bc1a7c <bikashjena2304@gmail.com>
2020-10-26 19:51:45 +0530

    Tokenizer beta

--- readGrammar.c
+++ readGrammar.c
@@ -3,3 +3,113 @@
  * that reads grammar from grammar.txt,
  * populates array of linked-list grammar G 
 */
+
+#include "structures.h"
+
+bool isterminal(char[]);
+bool isnonterminal(char[]);
+bool iskeyword(char[]);
+void insertGrammarToken(char grammar_token[], Grammar_Node *node);
+
+int num_of_keywords=0;
+
+Grammar readGrammar(char *filename, Grammar grammar)
+{
+	FILE *file = fopen(filename, "r");
+	
+	int counter = 0;
+	while (!feof(file))
+	{
+		grammar.rules[counter].num_of_nodes = 0;
+		char buffer[MAX_RULE_LEN], *grammar_token;
+		fscanf(file, "%[^\n]\n", buffer);
+
+		if (strcmp(buffer, "\n")==0)
+		{
+			continue;
+		}
+
+		grammar_token = strtok(buffer, " ");
+
+		insertGrammarToken(grammar_token, &grammar.rules[counter]);
+		grammar_token = strtok(NULL, " ");
+
+		Grammar_Node *temp = &grammar.rules[counter];
+		while (grammar_token != NULL)
+		{
+			grammar_token = strtok(NULL, " ");
+			if (grammar_token == NULL)
+			{
+				break;
+			}
+
+			temp->next_node = (Grammar_Node *)malloc(sizeof(Grammar_Node));
+			insertGrammarToken(grammar_token, temp->next_node);
+			grammar.rules[counter].num_of_nodes++;
+			temp->next_node->prev_node = temp;
+			temp = temp->next_node;
+		}
+		counter++;
+	}
+	grammar.num_of_rules = counter;
+	return grammar;
+	fclose(file);
+}
+
+void insertGrammarToken(char *grammar_token, Grammar_Node *node)
+{
+	if (isterminal(grammar_token))
+	{
+		grammar_token[strlen(grammar_token) - 1] = '\0';
+		grammar_token++;
+		strcpy(node->name, grammar_token);
+	}
+	else if (isnonterminal(grammar_token))
+	{
+		grammar_token[strlen(grammar_token) - 1] = '\0';
+		grammar_token++;
+		strcpy(node->name, grammar_token);
+	}
+	else if (iskeyword(grammar_token))
+	{
+		strcpy(node->name, "keyword");
+	}
+
+	strcpy(node->value, grammar_token);
+}
+
+bool isterminal(char a[])
+{
+	if (a[0] == '<')
+	{
+		return !(!(isupper(a[1])));
+	}
+	return false;
+}
+
+bool isnonterminal(char a[])
+{
+	if (a[0] == '<')
+	{
+		return !(isupper((int)a[1]));
+	}
+	return false;
+}
+
+bool iskeyword(char a[])
+{
+	if (a[0] != '<')
+	{
+		for (size_t i = 0; i < num_of_keywords; i++)
+		{
+			if (strcmp(keyword[i], a) == 0)
+			{
+				return true;
+			}
+		}
+		strcpy(keyword[num_of_keywords++], a);
+		return true;
+	}
+	else
+		return false;
+}
--- tokenizer.c
+++ tokenizer.c
@@ -3,76 +3,65 @@
  * which converts input code to tokens at S
 */
 
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "structures.h"
 
 #define MAX_LINE_LEN 100000
 
-int isKeyword(char *name);
-int isConstant(char *name);
+bool isKeyword(char *name);
 
 Token *tokenizer(FILE *source)
 {
 	Token head;
-	Token *tptr = &head;
+	Token *tok_ptr = &head;
 	char buffer[MAX_LINE_LEN];
 	char *token_name;
 	char *token_value;
 	int lines = 0;
 
-	while (source != EOF)
+	while (!feof(source))
 	{
 		lines++;
+		fscanf(source, "%[^\n]\n", buffer);
 
-		fscanf(source, "%[^\n]", buffer);
-		token_value = strtok(buffer, " ");
+		token_value = strtok(buffer, " \t");
 
-		while ((token_value = strtok(NULL, " ")) != NULL && lines != 1)
+		while (token_value != NULL)
 		{
-			tptr->next_node = (Token *)malloc(sizeof(Token));
-			tptr = tptr->next_node;
-			tptr->line_num = lines;
-			strcpy(tptr->value, token_value);
+			tok_ptr->next_node = (Token *)malloc(sizeof(Token));
+			tok_ptr = tok_ptr->next_node;
+			tok_ptr->line_num = lines;
+			strcpy(tok_ptr->value, token_value);
 
 			if (isKeyword(token_value))
 			{
-				strcpy(tptr->name, "keyword");
+				strcpy(tok_ptr->name, "keyword");
 			}
-			else if (isConstant(token_value))
+
+			else if (isdigit(*token_value))
 			{
-				strcpy(tptr->name, "constant");
+				strcpy(tok_ptr->name, "constant");
 			}
 			else
 			{
-				strcpy(tptr->name, "identifier");
+				strcpy(tok_ptr->name, "identifier");
 			}
-		}
-		
-	}
-	
 
+			token_value = strtok(NULL, " \t");
+		}
+	}
+
+	tok_ptr->next_node = NULL;
 	return head.next_node;
 }
 
-/* Represents each token as outputted by tokenizer
- * 
- * name: name of token
- * 
- * value: string value of token
- * 
- * line_num: tokens on same line in source code have same line number
- * 
- * next_node: pointer to next token
-
-typedef struct Token
+bool isKeyword(char *name)
 {
-	char name[MAX_NAME_LEN];
-	char value[MAX_VALUE_LEN];
-	int line_num;
-	struct Token *next_node;
-} Token;
-
-*/
+	for (size_t i = 0; i < num_of_keywords; i++)
+	{
+		if (strcmp(keyword[i], name) == 0)
+		{
+			return true;
+		}
+	}
+	return false;
+}
